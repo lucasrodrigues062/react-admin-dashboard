bbd59240979f714e9d0d66d1e84b37a6a16d2742
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2023-03-15 15:20:30 -0300

    Construido tema global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ColorModeContext, useMode } from "@/styles/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [theme, colorMode] = useMode();
+
+  return (
+    //@ts-ignore
+    <ColorModeContext.Provider value={colorMode}>
+      {/* @ts-ignore */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps}>
+          <main className="content"></main>
+        </Component>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
