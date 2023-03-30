4dfea8dbd6a2a07b27f265ac4c0eef47434c990d
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2023-03-30 17:26:25 -0300

    Criado formulario de novo usuario

--- src/scenes/global/Sidebar.tsx
+++ src/scenes/global/Sidebar.tsx
@@ -140,7 +140,7 @@ const Sidebar = ({
                 fontWeight="bold"
                 sx={{ m: "10px 0 0 0" }}
               >
-                Ed Roh
+                Lucas Rodrigues
               </Typography>
               <Typography variant="h5" color={colors.greenAccent[500]}>
                 VP Fancy Admin
